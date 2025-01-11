d918320b11a818508e45a2340357de3f0dc95a0d
ltk1903 <khangltk19032002@gmail.com>
2025-01-11 10:20:39 +0700

    add bootstrap to admin page

--- backend/app/javascript/application.js
+++ backend/app/javascript/application.js
@@ -1,1 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "popper"
+import "bootstrap"
+import "admin/sb-admin-2.min";
+import "./admin/demo/chart-area-demo";
+import "./admin/demo/chart-pie-demo"; 
